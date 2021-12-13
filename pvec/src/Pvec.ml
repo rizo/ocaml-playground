34c97501726e52c714a8d8a08a69b7b1f1bd4a0b
Rizo I <rizo@odis.io>
2021-12-13 19:47:36 +0000

    magic pvec

--- pvec/src/Pvec.ml
+++ pvec/src/Pvec.ml
@@ -7,8 +7,10 @@ let array_copy_and_add a x =
   new_a
 
 
+type node
+
 type 'a t = {
-  root : 'a node array;
+  root : node array;
   (* Root node links. *)
   tail : 'a array;
   (* A pointer to the tail of the vector. *)
@@ -17,13 +19,15 @@ type 'a t = {
   shift : int; (* Current maximum index shift. *)
 }
 
-and 'a node =
-  | Link of 'a node array (* Branch node with links to other nodes. *)
-  | Data of 'a array
-(* Leaf node holds the actual data values. *)
-
 let shift_by = 5
 let trie_len = int_of_float (2.0 ** float_of_int shift_by)
+
+let make_link = Obj.magic
+let make_data = Obj.magic
+
+let as_link = Obj.magic
+
+let as_data = Obj.magic
 
 (* O(1) *)
 let empty : 'a t = { root = [||]; len = 0; tail = [||]; shift = shift_by }
@@ -34,28 +38,18 @@ let singleton a = { root = [||]; len = 1; shift = shift_by; tail = [| a |] }
 (* O(1) *)
 let len v = v.len
 
-let link = function
-  | Link a -> a
-  | Data _ -> failwith "link request on data node"
-
-
-let data = function
-  | Data a -> a
-  | Link _ -> failwith "data request on link node"
-
-
 let tail_offset len =
   if len < 32 then 0 else ((len - 1) lsr shift_by) lsl shift_by
 
 
 let node_with_index v idx =
-  if idx >= tail_offset v.len then Data v.tail
+  if idx >= tail_offset v.len then make_data v.tail
   else
     let rec loop level node =
       if level = 0 then node
       else
         let sub_idx = (idx lsr level) land (trie_len - 1) in
-        loop (level - shift_by) (Array.get (link node) sub_idx)
+        loop (level - shift_by) (Array.get (as_link node) sub_idx)
     in
     loop (v.shift - shift_by) (Array.get v.root (idx lsr v.shift))
 
@@ -63,30 +57,33 @@ let node_with_index v idx =
 (* O(log32(n)) ~ O(1) *)
 let idx v idx =
   let node = node_with_index v idx in
-  Array.get (data node) (idx land (trie_len - 1))
+  Array.get (as_data node) (idx land (trie_len - 1))
 
 
 (* O(log32(n)) ~ O(1) *)
 let rec new_path level tail =
-  if level = 0 then Data tail else Link [| new_path (level - shift_by) tail |]
+  if level = 0 then make_data tail
+  else make_link [| new_path (level - shift_by) tail |]
 
 
 (* Pushes the tail array to its correct location in the tree.
    If the parent is a leaf node, add the tail as a Data node.
    If index maps to the existing child, extend the child with a Link to tail.
    Otherwise add a new child to parent with a tail node. *)
-let rec push_tail len level (parent : 'a node array) tail : 'a node array =
+let rec push_tail len level parent tail =
   let sub_idx = ((len - 1) lsr level) land (trie_len - 1) in
   (* Parent is a leaf node. *)
   if level = shift_by then
-    let target = Data tail in
+    let target = make_data tail in
     let parent' = array_copy_and_add parent target in
     parent'
     (* Maps to existing child.
        Replace the child with a link to target. *)
   else if sub_idx < Array.length parent then (
     let child = Array.get parent sub_idx in
-    let target = Link (push_tail len (level - shift_by) (link child) tail) in
+    let target =
+      make_link (push_tail len (level - shift_by) (as_link child) tail)
+    in
     let parent' = Array.copy parent in
     Array.set parent' sub_idx target;
     parent'
@@ -116,7 +113,7 @@ let add v x =
       len = v.len + 1;
       shift = v.shift + shift_by;
       tail = [| x |];
-      root = [| Link v.root; new_path v.shift v.tail |];
+      root = [| make_link v.root; new_path v.shift v.tail |];
     }
     (* Update the tree.
        Push the tail to the root. *)
