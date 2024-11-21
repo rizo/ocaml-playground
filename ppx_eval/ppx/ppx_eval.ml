d535c131ccec54c0d1f9717c91b566d80da03029
Rizo I <rizo@odis.io>
2024-11-21 10:25:07 +0000

    ppx_eval

--- ppx_eval/ppx/ppx_eval.ml
+++ ppx_eval/ppx/ppx_eval.ml
@@ -6,7 +6,7 @@ open Ppxlib
 
 let expand ~ctxt _input =
   let loc = Expansion_context.Extension.extension_point_loc ctxt in
-  let output = B.estring ~loc "HELLO" in
+  let output = B.estring ~loc "HELLO oo" in
   output
 
 
@@ -15,3 +15,5 @@ let extension =
     Ast_pattern.(single_expr_payload ( __))
     expand
 
+let () =
+  Ppxlib.Driver.V2.register_transformation "eval" ~extensions:[ extension ]
